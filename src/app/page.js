0aecca094961a670b09e91238ed8c89fdee836c3
Isaura Gutiérrez <66889989+Isaura234@users.noreply.github.com>
2023-12-29 22:59:38 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-5xl font-medium mb-4">Isaura Gutiérrez Vargas</h1>
       <h2 className="text-3xl font-medium mb-10">About me</h2>
       <div className="max-w-prose text-xl">
-        <p>Like many other Jekyll-based GitHub Pages templates, academicpages makes you separate the website’s content from its form. The content & metadata of your website are in structured markdown files, while various other files constitute the theme, specifying how to transform that content & metadata into HTML pages. You keep these various markdown (.md), YAML (.yml), HTML, and CSS files in a public GitHub repository. Each time you commit and push an update to the repository, the GitHub pages service creates static HTML pages based on these files, which are hosted on GitHub’s servers free of charge.</p>
+        <p> Statistician and Economist. My work focuses on data analysis, economic analysis, public economy and academic research. With keen interest in health economics, public policy and finance.</p>
       </div>
     </div>
   )
