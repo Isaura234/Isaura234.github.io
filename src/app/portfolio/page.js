ad77ece5d4cb37a14db23933b39376846fc64f26
Isaura Gutiérrez <66889989+Isaura234@users.noreply.github.com>
2023-12-30 09:14:17 -0600

    Update page.js

--- src/app/portfolio/page.js
+++ src/app/portfolio/page.js
@@ -6,25 +6,24 @@ export default function Page() {
       <h1 className="text-5xl font-medium mb-10">Portfolio</h1>
       <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-14">
         <PortfolioCard
-          title="Extracción de pigmentos naturales a partir de las plantas Justicia tinctoria y Bixa orellana como una alternativa al uso de tintes sintéticos en Costa Rica"
-          description="Los tintes sintéticos son muy populares debido a su bajo costo, aunque su uso se asocia a
-problemas de salud y medioambientales..."
+          title="Use of Factorial models"
+          description="Extraction of natural pigments from the plants Justicia tinctoria and Bixa orellana as an alternative to the use of synthetic dyes in Costa Rica."
           pdf="./Factorial analysis.pdf"
         />
         <PortfolioCard
-          title="¿Qué fue primero: la heterocedasticidad o normalidad? Un análisis del efecto de la heterocedasticidad sobre las pruebas de normalidad en un diseño experimental con dos factores sin interacción"
-          description="La heterocedasticidad surge cuando los datos no cumplen con el supuesto de homogeneidad de varianzas. El rompimiento de este supuesto puede conllevar a potencias más bajas o un incremento del error tipo I."
+          title="Simulation theory"
+          description="Use of simulation theory to test the impact of different degrees of heteroscedasticity on the assumed normality of the data"
           pdf="./Simulation Heteroscedasticity and Normality.pdf"
         />
         <PortfolioCard
-          title="Comparación de métodos de clasificación aplicados al mercado laboral costarricense"
-          description="El desempleo es uno de los problemas económicos que más afecta a los costarricenses. Al primer trimestre de 2023, la tasa de desempleo se ubicó en 12% y se ha mantenido por encima de 11% en los últimos años."
+          title="Classification methods"
+          description="Use of supervised learning models to classify the Costa Rican population into unemployed or employed categories using data from the Continuous Employment Survey (ECE)"
           pdf="./Classification methods.pdf"
           rFile="./desempleo.R"
         />
         <PortfolioCard
-          title="Efecto de un empujón de pertenencia social en estudiantes del curso de Introducción a la Economía de primer año de la carrera de Economía de la Universidad de Costa Rica"
-          description="La educación superior costarricense presenta tasas de deserción alarmantes, en el caso de la carrera de Economía de la Universidad de Costa Rica estas tasas han rondado el 50% en la última década, explicado en parte por el rendimiento académico en los cursos introductorios."
+          title="Mixed models"
+          description="Use of mixed models to analyze the impact of a nudge on the probability of belonging to the economics career at UCR."
           pdf="./Mixed models-Nudge analysis.pdf"
           rFile="./nudges.R"
         />
