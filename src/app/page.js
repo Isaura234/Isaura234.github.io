840670afe06e920fc15a4690e73e2efbdf601395
Isaura Gutiérrez <66889989+Isaura234@users.noreply.github.com>
2024-01-07 20:08:05 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,16 @@ export default function Home() {
       <h1 className="text-5xl font-medium mb-4">Isaura Gutiérrez Vargas</h1>
       <h2 className="text-3xl font-medium mb-10">About me</h2>
       <div className="max-w-prose text-xl">
-        <p> Statistician and Economist. My work focuses on data analysis, economic analysis, public economy and academic research. With keen interest in health economics, public policy and finance.</p>
+        <p> Statistician and Economist. 
+          My work focuses on data analysis, economic analysis, public economy and academic research. 
+
+1. I have experience in the development of econometric models 📊🔍📈.
+2. I possess skills in creating specialized visualizations 📊📉📊.
+3. I use simulation theory for detailed analysis 🔄💡📊.
+
+4. I apply models for experimental analysis 🧪🔍📈.
+
+5. I have been involved in the development of surveys and sampling 📋📊📝.</p>
       </div>
     </div>
   )
